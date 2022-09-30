16606ed05787dbd9131f6fbd177ead6e30628a10
Ismael Spaniol <ismaelspaniol@outlook.com>
2022-09-30 15:23:50 -0300

    ajuste visual

--- canivete/pages/files/workouts/[file_id]/[workouts_id]/exercises/index.tsx
+++ canivete/pages/files/workouts/[file_id]/[workouts_id]/exercises/index.tsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react"
 import { supabase, getCurrentUser } from '../../../../../../utils/supabase'
-
-import styles from '../../../../../../styles/Files.module.css'
+import styles from '../../../../../../styles/Exercises.module.css'
 import LoadingSpinner from "../../../../../../components/LoadingSpinner"
 import { useRouter } from "next/router";
 
+import Paper from '@mui/material/Paper';
+
+
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 
 export default  function Exercises (){
@@ -62,13 +71,14 @@ export default  function Exercises (){
         }
     }
   }
+  
+  
+
 
   return (        
     <>
-      {loading ? (<LoadingSpinner />) : (
-          <div className={styles.main}>
-    			<h1>Lista de Exercicios</h1>
-            	
+<div className={styles.main}>
+<h1>Lista de Exercicios</h1>            	
           <button  onClick={() => {
                                     router.push({
                                     pathname: '/files/workouts/[id_file]/[id_workouts]/exercises/[exercise_id]',
@@ -81,13 +91,30 @@ export default  function Exercises (){
                               }>
             Inserir Exercício
           </button>
-           
-            	<ul className={styles.list}>
-					{exercises.map((exercise : any) => (
-					<li key={exercise.id}>
-						
-                                            
-						<a href="#" onClick={() => {
+
+<TableContainer component={Paper}>
+      <Table sx={{ minWidth: 100 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Exercício</TableCell>
+            <TableCell align="right">Repetições</TableCell>
+            <TableCell align="right">Update</TableCell>
+            <TableCell align="right">Delete</TableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {exercises.map((exercise : any) => (
+            <TableRow
+              key={exercise.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row"> 
+                {exercise.exercise}
+              </TableCell>
+              <TableCell align="right">{exercise.repeat}</TableCell>              
+              <TableCell align="right">
+                <button onClick={() => {
                   router.push({
                     pathname: '/files/workouts/[id_file]/[id_workouts]/exercises/[exercise_id]',
                     query: { id_file: file_id,
@@ -95,26 +122,22 @@ export default  function Exercises (){
                             exercise_id : exercise.id 
                           },
                   })
-                  }}>Update
-                </a>		    
+                  }}>
+                  Update
+                </button>
+                </TableCell>
+              <TableCell  align="right">
+                  <button onClick={() => _handleRemoveExercise(exercise.id)}>                    
+                  {loading ? 'Gravando ...' : 'Remover'}                    
+                    </button>
+                  </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
 
-						<a href="#"
-						onClick={() => _handleRemoveExercise(exercise.id)}>
-							{loading ? 'Gravando ...' : 'Remover'}
-						</a> 
-
-       
-            <a>{exercise.exercise}  </a>
-
-					</li>                    
-                    
-					))}
-
-                    
-                </ul>   
-            </div>
-      )}   
-       
+    </div>
     </>            
   )
 }
